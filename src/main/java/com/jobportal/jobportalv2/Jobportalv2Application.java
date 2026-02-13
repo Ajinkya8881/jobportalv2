9447232ae4b409ac8de85276181704d17f9b922b
__white_walker__ <aj.kolhe9657@gmail.com>
2026-02-13 13:48:03 +0530

    Initial Spring Boot base setup with project structure and dependencies

---
+++ src/main/java/com/jobportal/jobportalv2/Jobportalv2Application.java
@@ -0,0 +1,13 @@
+package com.jobportal.jobportalv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jobportalv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jobportalv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jobportal/jobportalv2/Jobportalv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobportal.jobportalv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jobportalv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
